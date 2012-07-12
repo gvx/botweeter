5080436d1e7293839898de361a6feece97eb1d14
Robin Wellner <gyvox.public@gmail.com>
2012-07-12 23:20:02 +0200

    Simple implementation of full thingy

--- botweeter.py
+++ botweeter.py
@@ -2,6 +2,7 @@
 import socket
 import twitter
 import sys
+import StringIO
 
 def twitter2irc(intext):
 	if intext.startswith('/me '):
@@ -13,24 +14,39 @@ def irc2twitter(intext):
 		return '/me' + intext[7:-1]
 	return intext
 
+NEWLINE = '\r\n'
+
 def init(host, port, consumer_key, consumer_secret, access_token_key,
         access_token_secret):
     api = twitter.Api(consumer_key, consumer_secret,
             access_token_key, access_token_secret)
+	print api.VerifyCredentials()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     s.listen(1)
     conn, addr = s.accept()
     conn.settimeout(0.0)
+	buff = ''
+	nick = 'bot'
+	last_mention = None
     while True:
         try:
-            data = conn.recv(1024)
+            buff += conn.recv(1024)
         except socket.timeout:
             pass
         else:
-            #use twitter API
-            #send data back to bot
-            pass
+			if NEWLINE in buff:
+				index = buff.rindex(NEWLINE)
+				buff, lines = buff[index + 1:], buff[:index]
+				for line in lines.split(NEWLINE):
+					cmd, rest = line.split(' ', 1)
+					if cmd == 'NICK':
+						nick = rest
+					elif cmd == 'PRIVMSG':
+						name, rest = rest.split(':', 1)
+						api.PostUpdates('@%s %s' % (name.strip(), irc2twitter(rest)))
+		for status in api.GetMentions(last_mention):
+			conn.send(':%s PRIVMSG %s :%s%s' % (status.user.name, nick, twitter2irc(status.text[len(status.in_reply_to_screen_name)+3:]), NEWLINE))
     conn.close()
 
 
